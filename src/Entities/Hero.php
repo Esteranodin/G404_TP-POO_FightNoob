d44d362f4310ed903af22c7ab3d345f601ccc8c1
esteranodin <esteranodin@hotmail.fr>
2025-01-16 20:23:25 +0100

    ADD: HeroMapper

--- src/Entities/Hero.php
+++ src/Entities/Hero.php
@@ -9,10 +9,10 @@ class Hero
 
     // Méthode magique
 
-    public function __construct(int $idHero)
+    public function __construct(int $idHero, string $nameHero)
     {
         $this->idHero = $idHero;
-        // $this->nameHero = $nameHero;
+        $this->nameHero = $nameHero;
         // $this->pointsVieHero = $pointsVieHero;
     }
 
@@ -21,6 +21,11 @@ class Hero
     public function getIdHero()
     {
         return $this->idHero;
+    }
+
+    public function getNameHero()
+    {
+        return $this->nameHero;
     }
 
 
--- src/Mappers/HeroMapper.php
+++ src/Mappers/HeroMapper.php
@@ -1,6 +1,14 @@
 <?php
 
-//  class HeroMapper implements MapperContract
-// {
-   
-// }
+
+class HeroMapper implements MapperContract
+{
+    // Méthode statique pour mapper un tableau de données en objet Qcm
+    public static function mapToObject(array $heroDatas): Hero
+    {
+        return new Hero(  
+            $heroDatas["id"],
+            $heroDatas["name"]
+        );
+    }
+}
